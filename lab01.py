84102c00b028bb9dc4173ad57f5f63ed464cc162
Marcos <mlmarcosluan@gmail.com>
2024-06-09 16:56:04 -0300

    Adicionado o primeiro lab

---
+++ lab01.py
@@ -0,0 +1,18 @@
+###################################################
+# MC102 - Algoritmos e Programação de Computadores
+# Laboratório 1 - Aritmética com Inteiros
+# Nome: Marcos Luan Moraes de Oliveira
+# RA: 221532
+###################################################
+
+a = int(input())
+b = int(input())
+
+print("a =", a)
+print("b =", b)
+print("a + b =", a + b)
+print("a - b =", a - b)
+print("a * b =", a * b)
+print("a ** b =", a ** b)
+print("a // b =", a // b)
+print("a % b =", a % b)
